0e2e19c8da289502d786dd6135440d7f37141deb
sweetie <ivan.linzov@gmail.com>
2022-08-13 15:57:29 +0300

    added face recognition

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import cv2
 
-img = cv2.imread('test.jpg')
-print(img.shape)
-img = cv2.resize(img, (500, 500))
-print(img.shape)
+img = cv2.imread('D:/GB/bootcamp/ExploringOpenCV/test.jpg')
+
+#img = cv2.resize(img, (500, 500))
+
+cv2.imshow('Result', img)
+
+cv2.waitKey(0)
--- video.py
+++ video.py
@@ -1,10 +1,30 @@
 import cv2
 
+face_cascades = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+# img = cv2.imread('face.jpg')
+# img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# faces = face_cascades.detectMultiScale(img_gray)
+
+# for (x, y, w, h) in faces:
+#     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+
+# cv2.imshow('Result', img)
+# cv2.waitKey(0)
+
 cap = cv2.VideoCapture(0)
 
 while True:
     _, frame = cap.read()
-    cv2.imshow("camera", frame)
+    img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascades.detectMultiScale(img_gray)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    cv2.imshow('Result', frame)
 
     if cv2.waitKey(1) & 0xff == ord('q'):
         break
